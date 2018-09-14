14d03249ae995ba1e8b503b3e4b3f87cc917a22b
Ben Williams <8enwilliams@gmail.com>
2018-09-15 00:49:05 +1000

    version functions working

--- lib/sound_io.rb
+++ lib/sound_io.rb
@@ -3,23 +3,17 @@ require 'sound_io/device'
 require 'ffi'
 
 module SoundIO
-  extend FFI::Library
-  
-  # TODO:
-  # A pointer to a function that takes a pointer and an integer
-  # layout :function1, callback([:pointer, :int], :void)
+	extend FFI::Library
+	ffi_lib 'soundio'
+	
+	# libsoundio version number
+	attach_function :soundio_version_string, [], :string
+	attach_function :soundio_version_major, [], :int
+	attach_function :soundio_version_minor, [], :int
+	attach_function :soundio_version_patch, [], :int
 
-	class SoundIo < FFI::Struct
-		layout(
-			userdata: :pointer,
-			on_devices_change: :pointer,
-			on_backend_disconnect: :pointer,
-			on_events_signal: :pointer,
-			current_backend: :int,
-			app_name: :string,
-			emit_rtprio_warning: :pointer,
-			jack_info_callback: :pointer,
-			jack_error_callback: :pointer
-		)
-	end
+	# TODO: create the SoundIO struct 👇
+
+	# libsoundio instance creation
+	#attach_function :soundio_create, [], :pointer
 end
--- spec/sound_io_spec.rb
+++ spec/sound_io_spec.rb
@@ -1,5 +1,31 @@
 RSpec.describe SoundIO do
-  it 'has a version number' do
-    expect(SoundIO::VERSION).not_to be nil
+  describe '.VERSION' do
+    it 'returns a string' do
+      expect(SoundIO::VERSION).to be_instance_of(String)
+    end
+  end
+  
+  describe '#soundio_version_string' do
+    it 'returns a string' do
+      expect(SoundIO.soundio_version_string).to be_instance_of(String)
+    end
+  end
+  
+  describe '#soundio_version_major' do
+    it 'returns a fixnum' do
+      expect(SoundIO.soundio_version_major).to be_instance_of(Fixnum)
+    end
+  end
+  
+  describe '#soundio_version_minor' do
+    it 'returns a fixnum' do
+      expect(SoundIO.soundio_version_minor).to be_instance_of(Fixnum)
+    end
+  end
+  
+  describe '#soundio_version_patch' do
+    it 'returns a fixnum' do
+      expect(SoundIO.soundio_version_patch).to be_instance_of(Fixnum)
+    end
   end
 end
