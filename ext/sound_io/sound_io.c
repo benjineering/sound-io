f590474470849c129b11e52ae0e06696f7955cdc
Ben Williams <8enwilliams@gmail.com>
2018-10-13 20:30:59 +1100

    fix max channels

--- ext/sound_io/sound_io.c
+++ ext/sound_io/sound_io.c
@@ -3,11 +3,7 @@
 
 VALUE rb_mSoundIO = Qnil;
 
-VALUE max_channels() {
-  return UINT2NUM(SOUNDIO_MAX_CHANNELS);
-}
-
 void Init_sound_io() {
   rb_mSoundIO = rb_define_module("SoundIO");
-  rb_define_singleton_method(rb_mSoundIO, "MAX_CHANNELS", max_channels, 0);
+  rb_define_const(rb_mSoundIO, "MAX_CHANNELS", UINT2NUM(SOUNDIO_MAX_CHANNELS));
 }
